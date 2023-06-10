550ef4de3b4beac8bf73b0a5e66f56d1e4fbd66e
liuyouxun <lys8855220@gmail.com>
2023-06-10 12:24:06 +0800

    init SpringbootMember

---
+++ src/main/java/com/Group5Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group5Application.class, args);
+	}
+
+}
