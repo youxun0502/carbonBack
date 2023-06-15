ec2fe37d1d7760e083cbd607b5d11b46e2c1f043
liuyouxun <lys8855220@gmail.com>
2023-06-15 14:11:08 +0800

    email certified

--- src/main/java/com/liu/controller/MemberController.java
+++ src/main/java/com/liu/controller/MemberController.java
@@ -1,5 +1,27 @@
 package com.liu.controller;
 
-public class MemberController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.liu.service.MemberService;
+
+@Controller
+public class MemberController {
+	
+	@Autowired
+	MemberService mService;
+
+	@ResponseBody
+	@GetMapping("/member/api/checkEmail")
+	public String checkEmail(@RequestParam(name = "e") String email) {
+		boolean result = mService.emailAlreadyRegistered(email);
+		if(result == true) {
+			return "isExist";
+		}else {
+			return "isNotExist";
+		}
+	}
 }
