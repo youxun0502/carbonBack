95b6af1b5e68344ff3e86c0f6fdec6b574797310
liao <andy.liaotest2@gmail.com>
2023-06-26 16:44:27 +0800

    add photo

--- src/main/java/com/liao/controller/DiscussionsController.java
+++ src/main/java/com/liao/controller/DiscussionsController.java
@@ -1,18 +1,25 @@
 package com.liao.controller;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.liao.model.Discussions;
 import com.liao.service.DiscussionsService;
@@ -23,9 +30,32 @@ public class DiscussionsController {
 	@Autowired
 	private DiscussionsService dService;
 	
+	
+	//顯示圖片
+	@GetMapping("/discussiondownloadImage/{articleId}")
+	public ResponseEntity<byte[]> downloadImage(@PathVariable Integer articleId){
+		Discussions discussion = dService.findById(articleId);
+		byte[] photoFile = discussion.getPhotoFile();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		                                // 檔案, header, HttpStatus
+		return new ResponseEntity<byte[]>(photoFile, headers, HttpStatus.OK);
+	}
+	
+	@GetMapping("/goBackToMain")
+	public String goBackToMain() {
+		return "liao/main";
+	}
+	
 	@GetMapping("/discussions")
 	public String goHome() {
 		return "liao/DiscussionsMain";
+	}
+	
+	@GetMapping("/goBackToHome")
+	public String goBackToHome() {
+		return "liao/home";
 	}
 	
 	@GetMapping("/discussions/insertpage")
@@ -44,7 +74,9 @@ public class DiscussionsController {
 									@RequestParam("lastReplyTime") String lastReplyTime,
 									@RequestParam("d_views") Integer d_views,
 									@RequestParam("dcreated_at") String dcreated_at,
-									@RequestParam("dlikes") Integer dlikes, Model model) {
+									@RequestParam("dlikes") Integer dlikes,
+									@RequestParam("photoFile") MultipartFile photoFile,
+									Model model) throws IOException {
 		Discussions discussions = new Discussions();
 		discussions.setMemberId(memberId);
 		discussions.setEventId(eventId);
@@ -57,6 +89,7 @@ public class DiscussionsController {
 		discussions.setD_views(d_views);
 		discussions.setDcreated_at(dcreated_at);
 		discussions.setDlikes(dlikes);
+		discussions.setPhotoFile(photoFile.getBytes());
 		
 		dService.insert(discussions);
 		
