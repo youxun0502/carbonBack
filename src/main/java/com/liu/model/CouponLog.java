df908af06c7a5e31dab39c4359c4eee5d58a8a91
liuyouxun <lys8855220@gmail.com>
2023-06-29 13:17:58 +0800

    可以成功抽獎

--- src/main/java/com/liu/model/CouponLog.java
+++ src/main/java/com/liu/model/CouponLog.java
@@ -13,7 +13,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
