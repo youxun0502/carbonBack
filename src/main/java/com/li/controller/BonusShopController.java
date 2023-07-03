51fa0795370486c40006459ed596f7abb9c62f6d
jiaxianLi <ero3322001@gmail.com>
2023-07-03 10:16:00 +0800

    0703

--- src/main/java/com/li/controller/BonusShopController.java
+++ src/main/java/com/li/controller/BonusShopController.java
@@ -1,13 +1,16 @@
 package com.li.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.li.dto.BonusShopDto;
 import com.li.model.BonusItem;
@@ -48,6 +51,31 @@ public class BonusShopController {
 			return oneDto;
 		} else {
 			return null;
+		}
+	}
+	
+	@ResponseBody
+	@PostMapping("bonus/newBonusLog")
+	public String InsertBonusItemToDB(@RequestParam("bonusName") String bonusname,
+			@RequestParam("bonusPrice") Integer bonusPrice, @RequestParam("bonusDes") String bonusDes,
+			@RequestParam("status")Boolean status,
+			@RequestParam("img_file") MultipartFile file) {
+
+		try {
+			BonusItem bonusItem = new BonusItem();
+			bonusItem.setBonusName(bonusname);
+			bonusItem.setBonusPrice(bonusPrice);
+			bonusItem.setBonusDes(bonusDes);
+			bonusItem.setImg_file(file.getBytes());
+			bonusItem.setBonusImg(bonusname);
+			bonusItem.setStatus(status);
+			
+			bService.insertBonusItem(bonusItem);
+			
+			return "redirect:/bonus/listAll";
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "redirect:/bonus/main";
 		}
 	}
 
