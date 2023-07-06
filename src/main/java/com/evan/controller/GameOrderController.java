b1fde07859c7d729a9851c33c46fe6a717fe4724
EvanLu <coffeelululu@gmail.com>
2023-07-06 16:06:31 +0800

    0

--- src/main/java/com/evan/controller/GameOrderController.java
+++ src/main/java/com/evan/controller/GameOrderController.java
@@ -1,12 +1,10 @@
 package com.evan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.evan.service.GameOrderService;
-import com.ni.service.OrderService;
 
 @RestController
 public class GameOrderController {
