435273f31a369d2e1a8b61cd9ecc89b1326ff760
jiaxianLi <ero3322001@gmail.com>
2023-07-11 20:49:26 +0800

    0711

--- src/main/resources/static/js/profile-avataredit.js
+++ src/main/resources/static/js/profile-avataredit.js
@@ -4,6 +4,10 @@ const userlogin = document.getElementById('memberId');
 const avatar_box = document.getElementById('edit-avatar');
 const frame_box = document.getElementById('edit-frame');
 const bg_box = document.getElementById('edit-bg');
+let select_avatar = 0;
+let select_frame = 0;
+let select_bg = 0;
+
 
 let userid = 0;
 if (userlogin == null) {
@@ -40,7 +44,7 @@ function editboxMaker() {
 			console.log("res:" + res.data[0].bonusitem.bonusName);
 			console.log("length:" + res.data.length);
 			bonusLog = res.data;
-			let avatars;
+			let avatar_index = 0;
 			let frames;
 			let bgs;
 			let avatarhtml;
@@ -51,12 +55,20 @@ function editboxMaker() {
                 <div class="avatarcollection_SingleRow">`;
 			for (let i = 0; i < bonusLog.length; i++) {
 				if (bonusLog[i].bonusitem.bonusType == "avatar") {
+					if (avatar_index >= 4) {
+						avatarhtml += `</div>
+						<div class="avatarcollection_SingleRow">`;
+						avatar_index = 0;
+					}
 					avatarhtml += `						
-                        <div class="avatarcollection_AvatarPreview avatarcollection_Large">
-						<img  src="/carbon/downloadImage/${bonusLog[i].bonusitem.bonusId}">
-						</div>
-						<div class="avatarcollection_AvatarRowSpace"></div>
+						<div class="avatarcollection_AvatarPreview avatarcollection_Large">
+						<div class="select_avatar_frame">
+					<img class="avatar_select"  data-img=${bonusLog[i].bonusitem.bonusId} src="/carbon/downloadImage/${bonusLog[i].bonusitem.bonusId}">
+					</div>
+					</div>
 					`;
+					avatar_index++;
+					// <div class="avatarcollection_AvatarRowSpace avatar_select" data-img=${bonusLog[i].bonusitem.bonusId}></div>
 				}
 				else if (bonusLog[i].bonusitem.bonusType == "frame") {
 					frame_box.innerHTML += `
@@ -91,7 +103,7 @@ function editboxMaker() {
 			avatar_box.innerHTML = avatarhtml;
 
 
-
+			edit_select();
 			// 	avatar_box.innerHTML = `
 			// 	<div class="personal_avatar">
 			// 	<div class="personal_avatar_innersize">
@@ -110,4 +122,34 @@ function editboxMaker() {
 			console.log("err:" + err);
 		})
 
+}
+
+function edit_select() {
+	let avatar_select = document.getElementsByClassName('avatar_select');
+
+	for (let i = 0; i < avatar_select.length; i++) {
+		avatar_select[i].addEventListener('click', function (e) {
+			removeAvatarFrame();
+			console.log("avatar_select[" + i + "]:" + this.parentElement.innerHTML);
+			// 為目前點擊的 select_avatar_frame 添加 active 類別
+			// this.parentElement.classList.add('active_select');
+
+		})
+	}
+}
+
+function removeAvatarFrame() {
+	// 移除所有 select_avatar_frame 的 active 類別
+	let aframes = document.getElementsByClassName('select_avatar_frame');
+	console.log("aframes " + aframes[2].classList);
+	for (let j = 0; j < frames.length; j++) {
+		// 判斷是否為目前點擊的元素的父元素
+		if (aframes[j].parentNode === this.parentNode) {
+			console.log("aframes[j].classList.add('active');");
+			aframes[j].classList.add('active');
+		} else {
+			console.log("aframes[j].classList.remove('active');");
+			aframes[j].classList.remove('active');
+		}
+	}
 }
