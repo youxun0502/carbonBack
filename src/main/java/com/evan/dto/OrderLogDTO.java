d2f540cdb2921da6be4157451d7c86488a579ea3
EvanLu <coffeelululu@gmail.com>
2023-07-13 11:57:32 +0800

    photoId

--- src/main/java/com/evan/dto/OrderLogDTO.java
+++ src/main/java/com/evan/dto/OrderLogDTO.java
@@ -1,5 +1,10 @@
 package com.evan.dto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.evan.dao.GameRepository;
+import com.evan.model.GamePhoto;
+
 import lombok.Data;
 
 @Data
@@ -8,4 +13,19 @@ public class OrderLogDTO {
 	private String gameName;
 	
 	private Integer price;
+	
+	private Integer photoId;
+	
+	@Autowired
+	private GameRepository gRepos;
+
+	public Integer getPhotoId() {
+		
+		GamePhoto gamePhoto = gRepos.findGameByGameName(gameName).get(0).getGamePhotoLists().get(0);
+		photoId =gamePhoto.getPhotoId();
+		return photoId;
+	}
+
+	
+	
 }
